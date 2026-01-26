aa7ba25e92eea2810f8b50e83e26ba1f16453f9b
Abhishek_Jain <abhishekjain171103@gmail.com>
2026-01-26 16:34:56 +0530

    .

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="name@flowbite.com"
+              placeholder="Enter your email"
               required
             />
           </div>
@@ -74,6 +74,7 @@ const Login = () => {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               required
             />
