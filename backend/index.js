be30e70d619a0e9a42d9a9bd4ef8d2cad9556b59
Abhishek_Jain <abhishekjain171103@gmail.com>
2026-01-26 11:04:45 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(
     cors({
         origin: ["https://mini-hr-tool.netlify.app", "http://localhost:5173"],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
 );
